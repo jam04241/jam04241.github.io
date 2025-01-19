5aa1705e047773980aa354590a9c8baea2d974fe
Jam04241 <akog486@gmail.com>
2025-01-19 22:32:24 +0800

    added `about-index` and `contact-index`

---
+++ portfolio/main/about/about-script.js
@@ -0,0 +1,40 @@
+const chartData = {
+    labels: ["Html", "Css", "JavaScript","Java", "Python", "MySQL"],
+    data: [50, 50, 10, 25, 30, 40],
+  };
+  
+  const myChart = document.querySelector(".my-chart");
+  const ul = document.querySelector(".programming-stats .details ul");
+  
+  new Chart(myChart, {
+    type: "doughnut",
+    data: {
+      labels: chartData.labels,
+      datasets: [
+        {
+          label: "Language Popularity",
+          data: chartData.data,
+        },
+      ],
+    },
+    options: {
+      borderWidth: 10,
+      borderRadius: 2,
+      hoverBorderWidth: 0,
+      plugins: {
+        legend: {
+          display: false,
+        },
+      },
+    },
+  });
+  
+  const populateUl = () => {
+    chartData.labels.forEach((l, i) => {
+      let li = document.createElement("li");
+      li.innerHTML = `${l}: <span class='percentage'>${chartData.data[i]}%</span>`;
+      ul.appendChild(li);
+    });
+  };
+  
+  populateUl();
